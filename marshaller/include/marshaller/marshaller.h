ea656f153cbc71c188cc18e62a25bc55e310dfb7
Edward Boggis-Rolfe <edward@secretarium.com>
2022-01-21 22:56:24 +0000

    made i_unknown destructor virtual

--- marshaller/include/marshaller/marshaller.h
+++ marshaller/include/marshaller/marshaller.h
@@ -10,13 +10,17 @@ using error_code = int;
 class enclave_info;
 
 //the base interface to all interfaces
-class i_unknown{};
+class i_unknown
+{
+public:
+	virtual ~i_unknown(){}
+};
 
 //a shared pointer that works accross enclaves
 template<class T>class remote_shared_ptr
 {
 	T* the_interface = nullptr;
-	public:
+public:
 	remote_shared_ptr(){}
 	remote_shared_ptr(T* interface) : the_interface(interface){}
 	remote_shared_ptr<i_unknown>& as_i_unknown()
@@ -35,14 +39,14 @@ template<class T>class remote_weak_ptr{};
 //the used for marshalling data between zones
 class i_marshaller : public i_unknown
 {
-	public:
+public:
 	virtual error_code send(uint64_t object_id, uint64_t interface_id, uint64_t method_id, const yas::shared_buffer& in, yas::shared_buffer& out) = 0;
 	virtual error_code try_cast(i_unknown& from, uint64_t interface_id, remote_shared_ptr<i_unknown>& to) = 0;
 };
 
 class i_marshaller_impl : public i_marshaller
 {
-	public:
+public:
 	error_code send(uint64_t object_id, uint64_t interface_id, uint64_t method_id, const yas::shared_buffer& in, yas::shared_buffer& out) override;
 	error_code try_cast(i_unknown& from, uint64_t interface_id, remote_shared_ptr<i_unknown>& to) override;
 };
@@ -50,6 +54,7 @@ class i_marshaller_impl : public i_marshaller
 //a handler for new threads, this function needs to be thread safe!
 class i_thread_target : public i_unknown
 {
+public:
 	virtual error_code thread_started(std::string& thread_name) = 0;
 };
 
@@ -59,6 +64,7 @@ class i_message_channel : public i_unknown{};
 //a handler for new threads, this function needs to be thread safe!
 class i_message_target : public i_unknown
 {
+public:
 	//Set up a link with another zone
 	virtual error_code add_peer_channel(std::string link_name, i_message_channel& channel) = 0;
 	//This will be called if the other zone goes down
@@ -68,6 +74,7 @@ class i_message_target : public i_unknown
 //logical security environment
 class i_zone : public i_unknown
 {
+public:
 	//this runs until the thread dies, this will also setup a connection with the message pump
 	void start_thread(i_thread_target& target, std::string thread_name);
 	
