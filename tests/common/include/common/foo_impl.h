b9a0ad8603fe3f545197a0dc33c24a58a233b3e0
Edward Boggis-Rolfe <edward@secretarium.com>
2022-03-31 16:31:44 +0100

    fixed test

--- tests/common/include/common/foo_impl.h
+++ tests/common/include/common/foo_impl.h
@@ -13,141 +13,141 @@ namespace marshalled_tests
     {
     public:
         virtual ~foo() { }
-        int do_something_in_val(int val)
+        error_code do_something_in_val(int val)
         {
             log(std::string("got ") + std::to_string(val));
             return 0;
         }
-        int do_something_in_ref(const int& val)
+        error_code do_something_in_ref(const int& val)
         {
             log(std::string("got ") + std::to_string(val));
             return 0;
         }
-        int do_something_in_by_val_ref(const int& val)
+        error_code do_something_in_by_val_ref(const int& val)
         {
             log(std::string("got ") + std::to_string(val));
             return 0;
         }
-        int do_something_in_move_ref(int&& val)
+        error_code do_something_in_move_ref(int&& val)
         {
             log(std::string("got ") + std::to_string(val));
             return 0;
         }
-        int do_something_in_ptr(const int* val)
+        error_code do_something_in_ptr(const int* val)
         {
             log(std::string("got ") + std::to_string(*val));
             return 0;
         }
-        int do_something_out_val(int& val)
+        error_code do_something_out_val(int& val)
         {
             val = 33;
             return 0;
         };
-        int do_something_out_ptr_ref(int*& val)
+        error_code do_something_out_ptr_ref(int*& val)
         {
             val = new int(33);
             return 0;
         }
-        int do_something_out_ptr_ptr(int** val)
+        error_code do_something_out_ptr_ptr(int** val)
         {
             *val = new int(33);
             return 0;
         }
-        int do_something_in_out_ref(int& val)
+        error_code do_something_in_out_ref(int& val)
         {
             log(std::string("got ") + std::to_string(val));
             val = 33;
             return 0;
         }
-        int give_something_complicated_val(const xxx::something_complicated val)
+        error_code give_something_complicated_val(const xxx::something_complicated val)
         {
             log(std::string("got ") + std::to_string(val.int_val));
             return 0;
         }
-        int give_something_complicated_ref(const xxx::something_complicated& val)
+        error_code give_something_complicated_ref(const xxx::something_complicated& val)
         {
             log(std::string("got ") + std::to_string(val.int_val));
             return 0;
         }
-        int give_something_complicated_ref_val(const xxx::something_complicated& val)
+        error_code give_something_complicated_ref_val(const xxx::something_complicated& val)
         {
             log(std::string("got ") + std::to_string(val.int_val));
             return 0;
         }
-        int give_something_complicated_move_ref(xxx::something_complicated&& val)
+        error_code give_something_complicated_move_ref(xxx::something_complicated&& val)
         {
             log(std::string("got ") + std::to_string(val.int_val));
             return 0;
         }
-        int give_something_complicated_ptr(const xxx::something_complicated* val)
+        error_code give_something_complicated_ptr(const xxx::something_complicated* val)
         {
             log(std::string("got ") + std::to_string(val->int_val));
             return 0;
         }
-        int recieve_something_complicated_ref(xxx::something_complicated& val)
+        error_code recieve_something_complicated_ref(xxx::something_complicated& val)
         {
             val = xxx::something_complicated {33, "22"};
             return 0;
         }
-        int recieve_something_complicated_ptr(xxx::something_complicated*& val)
+        error_code recieve_something_complicated_ptr(xxx::something_complicated*& val)
         {
             val = new xxx::something_complicated {33, "22"};
             return 0;
         }
-        int recieve_something_complicated_in_out_ref(xxx::something_complicated& val)
+        error_code recieve_something_complicated_in_out_ref(xxx::something_complicated& val)
         {
             log(std::string("got ") + std::to_string(val.int_val));
             val.int_val = 33;
             return 0;
         }
-        int give_something_more_complicated_val(const xxx::something_more_complicated val)
+        error_code give_something_more_complicated_val(const xxx::something_more_complicated val)
         {
             log(std::string("got ") + val.map_val.begin()->first);
             return 0;
         }
-        int give_something_more_complicated_ref(const xxx::something_more_complicated& val)
+        error_code give_something_more_complicated_ref(const xxx::something_more_complicated& val)
         {
             log(std::string("got ") + val.map_val.begin()->first);
             return 0;
         }
-        int give_something_more_complicated_move_ref(xxx::something_more_complicated&& val)
+        error_code give_something_more_complicated_move_ref(xxx::something_more_complicated&& val)
         {
             log(std::string("got ") + val.map_val.begin()->first);
             return 0;
         }
-        int give_something_more_complicated_ref_val(const xxx::something_more_complicated& val)
+        error_code give_something_more_complicated_ref_val(const xxx::something_more_complicated& val)
         {
             log(std::string("got ") + val.map_val.begin()->first);
             return 0;
         }
-        int give_something_more_complicated_ptr(const xxx::something_more_complicated* val)
+        error_code give_something_more_complicated_ptr(const xxx::something_more_complicated* val)
         {
             log(std::string("got ") + val->map_val.begin()->first);
             return 0;
         }
-        int recieve_something_more_complicated_ref(xxx::something_more_complicated& val)
+        error_code recieve_something_more_complicated_ref(xxx::something_more_complicated& val)
         {
             val.map_val["22"] = xxx::something_complicated {33, "22"};
             return 0;
         }
-        int recieve_something_more_complicated_ptr(xxx::something_more_complicated*& val)
+        error_code recieve_something_more_complicated_ptr(xxx::something_more_complicated*& val)
         {
             val = new xxx::something_more_complicated();
             val->map_val["22"] = xxx::something_complicated {33, "22"};
             return 0;
         }
-        int recieve_something_more_complicated_in_out_ref(xxx::something_more_complicated& val)
+        error_code recieve_something_more_complicated_in_out_ref(xxx::something_more_complicated& val)
         {
             log(std::string("got ") + val.map_val.begin()->first);
             val.map_val["22"] = xxx::something_complicated {33, "23"};
             return 0;
         }
-        int do_multi_val(int val1, int val2)
+        error_code do_multi_val(int val1, int val2)
         {
             log(std::string("got ") + std::to_string(val1));
             return 0;
         }
-        int do_multi_complicated_val(const xxx::something_more_complicated val1, const xxx::something_more_complicated val2)
+        error_code do_multi_complicated_val(const xxx::something_more_complicated val1, const xxx::something_more_complicated val2)
         {
             log(std::string("got ") + val1.map_val.begin()->first);
             return 0;
