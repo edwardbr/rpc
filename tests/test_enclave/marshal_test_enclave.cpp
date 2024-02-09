61a34742d17f2db92f2010ee50c7e067ca7a4b53
Edward Boggis-Rolfe <edward@secretarium.com>
2024-02-09 23:24:02 +0000

    implemented better isolation of ENCLAVE_TELEMETRY_SERVICE

--- tests/test_enclave/marshal_test_enclave.cpp
+++ tests/test_enclave/marshal_test_enclave.cpp
@@ -14,7 +14,11 @@
 #include <example/example.h>
 
 #include <rpc/remote_pointer.h>
+
+#include <rpc/telemetry/i_telemetry_service.h>
+#ifdef USE_RPC_TELEMETRY
 #include <rpc/telemetry/enclave_telemetry_service.h>
+#endif
 
 using namespace marshalled_tests;
 
